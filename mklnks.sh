04b523393c6bc085e3dcc620bfe71b6ab83086db
Previnder <git@previnder.com>
2024-08-07 10:19:29 +0530

    Include VSCode user settings

--- mklnks.sh
+++ mklnks.sh
@@ -10,8 +10,15 @@ ln -fsr "nvim/init.vim" "$NVIM_DIR/init.vim"
 
 MPV_DIR="$HOME/.config/mpv"
 mkdir -p $MPV_DIR
-ln -fsr "mpv/*" "$NVIM_DIR/"
+ln -fsr "mpv/input.conf" "$MPV_DIR/input.conf"
+ln -fsr "mpv/mpv.conf" "$MPV_DIR/mpv.conf"
 
 ZATHURA_DIR="$HOME/.config/mpv"
 mkdir -p $ZATHURA_DIR
 ln -fsr "zathurarc" "$ZATHURA_DIR/zathurarc"
+
+VSCODE_DIR="$HOME/.config/Code/User"
+mkdir -p $VSCODE_DIR
+ln -fsr "vscode/settings.json" "$VSCODE_DIR/settings.json"
+ln -fsr "vscode/keybindings.json" "$VSCODE_DIR/keybindings.json"
+
